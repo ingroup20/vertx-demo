9353324b74ccda374944ade6bd4b1c9d3fbfc88d
chuckwang <chuckwang@gateweb.com.tw>
2024-08-09 19:19:28 +0800

    rewrite onComplete() to onSuccess()

--- src/main/java/com/example/starterVertx/MainVerticle.java
+++ src/main/java/com/example/starterVertx/MainVerticle.java
@@ -13,30 +13,39 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-
+    // Create a Router
     Router router = Router.router(vertx);
 
-    router.route().handler(context ->{
+    // Mount the handler for all incoming requests at every path and HTTP method
+    router.route().handler(context -> {
+      // Get the address of the request
       String address = context.request().connection().remoteAddress().toString();
+      // Get the query parameter "name"
       MultiMap queryParams = context.queryParams();
-      String name = queryParams.contains("name")?queryParams.get("name"):"unkown";
+      String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
+      // Write a json response
       context.json(
         new JsonObject()
-          .put("name",name)
-          .put("address",address)
-          .put("message","Hello "+name+" from "+address)
+          .put("name", name)
+          .put("address", address)
+          .put("message", "Hello " + name + " connected from " + address)
       );
     });
-
     //can run on port 8888 and test with http://localhost:8888/?name=myName
 
-    vertx.createHttpServer().requestHandler(router).listen(8808).onComplete(http -> {
-      if (http.succeeded()) {
-        startPromise.complete();//自動印出成功啟動資訊
-        System.out.println("HTTP server started on port "+http.result().actualPort());
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+
+    // Create the HTTP server
+    vertx.createHttpServer()
+      // Handle every request using the router
+      .requestHandler(router)
+      // Start listening
+      .listen(8888)
+      // Print the port
+      .onSuccess(server ->
+        System.out.println("HTTP server started on port " + server.actualPort())
+      )
+      .onFailure(throwable -> {
+        System.out.println("HTTP server failed to start");
+      });
   }
 }
